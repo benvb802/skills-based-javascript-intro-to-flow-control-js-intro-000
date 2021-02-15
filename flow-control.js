ae29ce4a0026419df6f19c65313c8b9fa7f5537e
Ben VB <bclvb802@gmail.com>
2021-02-15 18:48:03 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -6,9 +6,9 @@ if (13-19){
 
 function teenager(age) {
   if (13-19) {
-    return "You are not a teenager"
+    return "You are a teenager!"
 } else {
-  return "You are a teenager!"
+  return "You are not a teenager"
 }
 }
 
